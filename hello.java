935f3512765062af037369923b267277e7902ffd
jaejungjun <jjj_0916@naver.com>
2015-12-22 09:35:44 +0900

    "That`s DVCS"

--- hello.java
+++ hello.java
@@ -1,6 +1,8 @@
 public class hello{
 	public static void main(String[] args){
-				System.out.println("hello git!");
-				System.out.println("what is git?");
+				System.out.println("Hello git!");
+				System.out.println("What is git?");
 					}	
+					System.out.println("that`s DVCS");
+
 }		
