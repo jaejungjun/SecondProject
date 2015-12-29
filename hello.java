d520b8ad7be91df065de50004df016a6dd1a40ea
jaejungjun <jjj_0916@naver.com>
2015-12-29 09:27:43 +0900

    github for git pull

--- hello.java
+++ hello.java
@@ -6,6 +6,7 @@ public class hello{
 				System.out.println("Hello git!");
 				System.out.println("What is git?");
 				System.out.println("2002 BitKeeper");
-				System.out.println("that`s DVCS");
+				System.out.println("That`s DVCS");
+				System.out.println("That`s version control");
 
 }	}	
